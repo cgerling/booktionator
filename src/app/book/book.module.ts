7836e7dfc0ef4391c8ac44d29dbfbdeeba560922
Calvin Gerling <calvingerling@outlook.com>
2017-06-24 00:30:11 -0300

    feat(book): add book module basic structure
    #15

---
+++ src/app/book/book.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { BookRoutingModule } from './book.routing.module';
+
+@NgModule({
+  imports: [BookRoutingModule],
+  declarations: [],
+  providers: [],
+  exports: []
+})
+export class BookModule { }
