e1cbd86db90ce0f14b6f581a682a12711140c545
Calvin Gerling <calvingerling@outlook.com>
2017-06-24 00:29:20 -0300

    feat(access): add access module basic structure
    #15

---
+++ src/app/access/acess.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { AccessRoutingModule } from './access.routing.module';
+
+@NgModule({
+  imports: [AccessRoutingModule],
+  declarations: [],
+  providers: [],
+  exports: []
+})
+export class AccessModule { }
