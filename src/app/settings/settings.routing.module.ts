2f592fe1b35f601ef301757833920cb18c9306a0
Calvin Gerling <calvingerling@outlook.com>
2017-06-24 00:31:13 -0300

    feat(settings): add settings module basic structure
    #15

---
+++ src/app/settings/settings.routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: undefined,
+    children: []
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: []
+})
+export class SettingsRoutingModule { }
