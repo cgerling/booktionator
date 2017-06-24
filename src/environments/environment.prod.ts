e61651e70cf52283a87456a421d7a322d7d06562
Calvin Gerling <calvingerling@outlook.com>
2017-06-24 06:53:37 -0300

    fix(auth): add firebase data to prod environment
    #14

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBRbJsGQlASaHn9rQhkmYpkMqZ9rJ3FMrM',
+    authDomain: 'booktionator.firebaseapp.com',
+    databaseURL: 'https://booktionator.firebaseio.com',
+    projectId: 'booktionator',
+    storageBucket: 'booktionator.appspot.com'
+  }
 };
