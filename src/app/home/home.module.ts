0af0fa9c30c0e4770c17b14606353c77f00e7f77
Calvin Gerling <calvingerling@outlook.com>
2017-06-24 00:30:41 -0300

    feat(home): add home module basic structure
    #15

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { HomeRoutingModule } from './home.routing.module';
+
+@NgModule({
+  imports: [HomeRoutingModule],
+  declarations: [],
+  providers: [],
+  exports: []
+})
+export class HomeModule { }
