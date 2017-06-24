758bdbd4ab5656a5ee2c5b558b01de025cc0ea07
Calvin Gerling <calvingerling@outlook.com>
2017-06-24 06:26:05 -0300

    feat(firebase-auth): add configuration of auth
    #14

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
 import { AppRoutingModule } from 'app/app-routing.module';
 import { SharedModule } from 'app/shared/shared.module';
 
 import { AppComponent } from './app.component';
 
+import { environment } from 'environments/environment';
+
 @NgModule({
   imports: [
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(environment.firebase),
     AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,12 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBRbJsGQlASaHn9rQhkmYpkMqZ9rJ3FMrM',
+    authDomain: 'booktionator.firebaseapp.com',
+    databaseURL: 'https://booktionator.firebaseio.com',
+    projectId: 'booktionator',
+    storageBucket: 'booktionator.appspot.com'
+  }
 };
