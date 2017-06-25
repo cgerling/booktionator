f4f520b85c13622951f1e30637639a4aed6d57ba
Calvin Gerling <calvingerling@outlook.com>
2017-06-25 01:00:25 -0300

    refactor(shared): declare AuthGuard as provider

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,13 +4,16 @@ import { RouterModule } from '@angular/router';
 import { MdButtonModule, MdIconModule, MdMenuModule, MdToolbarModule } from '@angular/material';
 
 import { NavbarComponent } from './components/navbar/navbar.component';
+
 import { AuthService } from './services/auth.service';
 import { StorageService } from './services/storage.service';
+
+import { AuthGuard } from './guards/auth.guard';
 
 @NgModule({
   imports: [CommonModule, MdButtonModule, MdIconModule, MdMenuModule, MdToolbarModule, RouterModule],
   declarations: [NavbarComponent],
-  providers: [AuthService, StorageService],
+  providers: [AuthGuard, AuthService, StorageService],
   exports: [NavbarComponent]
 })
 export class SharedModule { }
