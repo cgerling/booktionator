df434fc4d6fc94d4218221430177a9da531538da
Calvin Gerling <calvingerling@outlook.com>
2017-06-25 00:59:59 -0300

    fix(shared): import RouterModule
    Import module to routerLink attributes work properly

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { MdButtonModule, MdIconModule, MdMenuModule, MdToolbarModule } from '@angular/material';
 
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -7,7 +8,7 @@ import { AuthService } from './services/auth.service';
 import { StorageService } from './services/storage.service';
 
 @NgModule({
-  imports: [CommonModule, MdButtonModule, MdIconModule, MdMenuModule, MdToolbarModule],
+  imports: [CommonModule, MdButtonModule, MdIconModule, MdMenuModule, MdToolbarModule, RouterModule],
   declarations: [NavbarComponent],
   providers: [AuthService, StorageService],
   exports: [NavbarComponent]
