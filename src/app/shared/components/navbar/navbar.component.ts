074061698f98694acbb9e7d9e81edb733cc5a469
Calvin Gerling <calvingerling@outlook.com>
2017-07-12 19:43:14 -0300

    refactor(navbar): undo the @input renaming

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -6,26 +6,35 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-  @Input('withSearch')
-  withSearch: boolean = true;
-  @Input('isLogged')
-  isLogged: boolean = false;
-  @Input('loggedUser')
-  username: string = '';
-  @Input('actualPage')
-  pageName: string = '';
+  @Input()
+  withSearch: boolean;
+  @Input()
+  isLogged: boolean;
+  @Input()
+  loggedUser: string;
+  @Input()
+  actualPage: string;
 
-  pageRoute: string = 'access/login';
-  accessText: string = 'Sign in';
+  pageRoute: string;
+  accessText: string;
+
+  constructor() {
+    this.withSearch = true;
+    this.isLogged = true;
+    this.loggedUser = '';
+    this.actualPage = '';
+    this.pageRoute = 'access/login';
+    this.accessText = 'Sign in';
+  }
 
   ngOnInit(): void {
-    this.withSearch = this.withSearch.toString() == 'true';
-    this.isLogged = this.isLogged.toString() == 'true';
+    this.withSearch = this.withSearch.toString() === 'true';
+    this.isLogged = this.isLogged.toString() === 'true';
 
-    if (this.pageName.toLowerCase() === 'login') {
+    if (this.actualPage.toLowerCase() === 'login') {
       this.accessText = 'Sign up';
       this.pageRoute = '/access/register';
-    } else if (this.pageName.toLowerCase() === 'register') {
+    } else if (this.actualPage.toLowerCase() === 'register') {
       this.accessText = 'Sign in';
       this.pageRoute = '/access/login';
     }
