a2e11cedfd12977ff38004576c9134ba6c38d200
Calvin Gerling <calvingerling@outlook.com>
2017-07-12 21:21:01 -0300

    refactor(navbar): improve component reliability
    Instead getting input values of tag watch routes and use the auth service to get the current user if there's one

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,42 +1,71 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+import { Router, NavigationStart, NavigationEnd, NavigationCancel, NavigationError } from '@angular/router';
+import { AuthService } from 'app/shared/services/auth.service';
+
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
+  private auth: AuthService;
+  private router: Router;
+
+  private routeSubs: Subscription;
+
   @Input()
   withSearch: boolean;
   @Input()
-  isLogged: boolean;
-  @Input()
-  loggedUser: string;
-  @Input()
   actualPage: string;
 
+  isLogged: boolean;
+  loggedUser: string;
   pageRoute: string;
   accessText: string;
 
-  constructor() {
+  constructor(auth: AuthService, router: Router) {
+    this.auth = auth;
+    this.router = router;
+
     this.withSearch = true;
-    this.isLogged = true;
-    this.loggedUser = '';
-    this.actualPage = '';
-    this.pageRoute = 'access/login';
-    this.accessText = 'Sign in';
+    this.pageRoute = 'access/register';
+    this.accessText = 'Sign up';
   }
 
   ngOnInit(): void {
     this.withSearch = this.withSearch.toString() === 'true';
-    this.isLogged = this.isLogged.toString() === 'true';
+    this.isLogged = this.auth.isLogged();
+    this.loggedUser = this.isLogged ? this.auth.currentUser().displayName : undefined;
 
-    if (this.actualPage.toLowerCase() === 'login') {
+    this.updateButton(this.router.url);
+
+    this.routeSubs = this.router.events.subscribe(this.routeHandle.bind(this));
+  }
+
+  ngOnDestroy(): void {
+    this.routeSubs.unsubscribe();
+  }
+
+  navigate(): void {
+    this.router.navigate([this.pageRoute]);
+  }
+
+  private routeHandle(value: NavigationStart | NavigationEnd | NavigationCancel | NavigationError): void {
+    this.updateButton(value.url);
+  }
+
+  private updateButton(actualRoute: string): void {
+    let route: string[] = actualRoute.split('/');
+    let actualPage: string = route[route.length - 1];
+
+    if (actualPage === 'login') {
       this.accessText = 'Sign up';
-      this.pageRoute = '/access/register';
-    } else if (this.actualPage.toLowerCase() === 'register') {
+      this.pageRoute = 'access/register';
+    } else if (actualPage === 'register') {
       this.accessText = 'Sign in';
-      this.pageRoute = '/access/login';
+      this.pageRoute = 'access/login';
     }
   }
 }
