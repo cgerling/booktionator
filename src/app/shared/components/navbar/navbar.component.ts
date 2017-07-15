468ca43fe78cbcc4c2b0f25a9eb6067e57f80294
Calvin Gerling <calvingerling@outlook.com>
2017-07-15 08:12:04 -0300

    fix(navbar): create element shadow
    Create a element with same dimensions of navbar component to occupy space on screen and don't overlap with page content

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -7,7 +7,7 @@ import { Subscription } from 'rxjs/Subscription';
 @Component({
   selector: 'navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit, OnDestroy {
   private auth: AuthService;
