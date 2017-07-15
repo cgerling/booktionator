9868497e03dc7070505d5504adb46f71752c0a7f
Calvin Gerling <calvingerling@outlook.com>
2017-07-15 08:33:25 -0300

    refactor(login): change the password toggle mechanism
    use the brand new password toggle component in the password input

--- src/app/access/login/login.component.ts
+++ src/app/access/login/login.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { AuthService } from 'app/shared/services/auth.service';
 
 import { User } from 'firebase/app';
+import { PasswordToggleComponent } from 'app/shared/components/password-toggle/password-toggle.component';
 
 @Component({
   selector: 'login',
@@ -14,36 +15,20 @@ export class LoginComponent {
   private service: AuthService;
   private router: Router;
 
+  @ViewChild(PasswordToggleComponent)
+  toggler: PasswordToggleComponent;
+
   email: string;
   password: string;
-
-  passwordMode: string;
-  passwordType: string;
 
   constructor(service: AuthService, router: Router) {
     this.service = service;
     this.router = router;
-
-    this.passwordMode = 'visibility';
-    this.passwordType = 'password';
   }
 
   login(): void {
     this.service.login(this.email, this.password).then((user: User) => {
       this.router.navigate(['/home']);
     });
-  }
-
-  togglePassword(): void {
-    const visibility = 'visibility', visibility_off = 'visibility_off';
-    const text = 'text', password = 'password';
-
-    if (this.passwordType === password) {
-      this.passwordMode = visibility_off;
-      this.passwordType = text;
-    } else if (this.passwordType === text) {
-      this.passwordMode = visibility;
-      this.passwordType = password;
-    }
   }
 }
