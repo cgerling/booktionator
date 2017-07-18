fb760a68c88544b3e69db950f7770ae022f22a82
Calvin Gerling <calvingerling@outlook.com>
2017-07-18 17:42:51 -0300

    feat(access): add a progress bar
    add a progress bar to show whatever needs a progress and use the service to link with access children

--- src/app/access/access.component.ts
+++ src/app/access/access.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+
+import { LoaderService } from 'app/shared/services/loader.service';
+import { Subscription } from "rxjs/Subscription";
 
 @Component({
   selector: 'access',
   templateUrl: './access.component.html',
   styleUrls: ['./access.component.scss']
 })
-export class AccessComponent {
+export class AccessComponent implements OnDestroy {
+  loading: boolean;
 
+  private subscriber: Subscription;
+
+  constructor(loaderService: LoaderService) {
+    this.loading = false;
+
+    this.subscriber = loaderService.loading$.subscribe(this.onLoad.bind(this));
+  }
+
+  ngOnDestroy(): void {
+    this.subscriber.unsubscribe();
+  }
+
+  onLoad(isLoading: boolean): void {
+    this.loading = isLoading;
+  }
 }
--- src/app/access/access.module.ts
+++ src/app/access/access.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { AccessRoutingModule } from './access.routing.module';
 
 import { FormsModule } from '@angular/forms';
-import { MdButtonModule, MdIconModule, MdInputModule, MdSnackBarModule } from '@angular/material';
+import { MdButtonModule, MdIconModule, MdInputModule, MdProgressBarModule, MdSnackBarModule } from '@angular/material';
 import { SharedModule } from 'app/shared/shared.module';
 
 import { AccessComponent } from './access.component';
@@ -11,7 +12,7 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  imports: [AccessRoutingModule, CommonModule, FormsModule, MdButtonModule, MdIconModule, MdInputModule, MdSnackBarModule, SharedModule],
+  imports: [AccessRoutingModule, CommonModule, FormsModule, MdButtonModule, MdIconModule, MdInputModule, MdProgressBarModule, MdSnackBarModule, SharedModule],
   declarations: [AccessComponent, LoginComponent, RegisterComponent],
   providers: [],
   exports: []
