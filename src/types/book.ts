92f1d84babde31cf5c694d878bd557784c6cecd1
Diogo Korok <korokdiogo@gmail.com>
2017-07-19 23:06:28 -0300

    refactor(types): change uuid to uid & add related image URL
    create getter for image URL, update constructor & update uid getter

--- src/types/book.ts
+++ src/types/book.ts
@@ -1,22 +1,30 @@
 import { Author } from 'types/author';
 
 export class Book {
+  private _uid: string;
   private _title: string;
   private _synopsis: string;
-  private _author: Author;
+  private _author: string;
   private _year: number;
   private _score: number;
+  private _imageUrl: string;
 
   static validate(title: string, year: number): boolean {
     return title !== '' && Number.isInteger(year);
   }
 
-  constructor(title: string, synopsis: string, author: Author, year: number, score: number = 0.0) {
+  constructor(uid: string, title: string, synopsis: string = '', author: string, year: number, score: number = 0.0, imageUrl: string = '') {
+    this._uid = uid;
     this._title = title;
     this._synopsis = synopsis;
     this._author = author;
     this._year = year;
     this._score = score;
+    this._imageUrl = imageUrl;
+  }
+  
+  get uid(): string {
+    return this._uid;
   }
 
   get title(): string {
@@ -27,7 +35,7 @@ export class Book {
     return this._synopsis;
   }
 
-  get author(): Author {
+  get author(): string {
     return this._author;
   }
 
@@ -37,6 +45,10 @@ export class Book {
 
   get score(): number {
     return this._score;
+  }
+  
+  get imageUrl(): string {
+    return this._imageUrl;
   }
 
   get valid(): boolean {
