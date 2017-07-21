4c8fdb6a8651b389c58c3fe377c979caab57580e
Calvin Gerling <calvingerling@outlook.com>
2017-07-21 14:26:05 -0300

    feat(access): add handle to query parameters
    implement on component the handler to the link in the firebase email

--- src/app/access/access.component.ts
+++ src/app/access/access.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 import { LoaderService } from 'app/shared/services/loader.service';
+import { AuthService } from 'app/shared/services/auth.service';
+
 import { Subscription } from "rxjs/Subscription";
 
 @Component({
@@ -8,15 +11,46 @@ import { Subscription } from "rxjs/Subscription";
   templateUrl: './access.component.html',
   styleUrls: ['./access.component.scss']
 })
-export class AccessComponent implements OnDestroy {
+export class AccessComponent implements OnDestroy, OnInit {
   loading: boolean;
 
+  private auth: AuthService;
+  private route: ActivatedRoute;
+  private router: Router;
   private subscriber: Subscription;
 
-  constructor(loaderService: LoaderService) {
+  constructor(auth: AuthService, loaderService: LoaderService, route: ActivatedRoute, router: Router) {
+    this.auth = auth;
+    this.route = route;
+    this.router = router;
+
     this.loading = false;
 
     this.subscriber = loaderService.loading$.subscribe(this.onLoad.bind(this));
+  }
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params: Params) => {
+      let { mode, oobCode } = params;
+      let route;
+
+      if (!oobCode) return;
+
+      switch (mode) {
+        case 'resetPassword':
+          route = '/access/password-reset';
+          break;
+        case 'recoverEmail':
+          break;
+        case 'verifyEmail':
+          route = '/access/verify-email';
+          break;
+        default:
+        //TODO: show error of invalid url
+      }
+
+      this.router.navigate([route, oobCode]);
+    });
   }
 
   ngOnDestroy(): void {
