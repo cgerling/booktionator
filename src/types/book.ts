1ffb8b9fa8ff9ea4fc9b41f1b829ac246797f5a5
Calvin Gerling <calvingerling@outlook.com>
2017-07-23 01:07:08 -0300

    feat(book): add publisher info

--- src/types/book.ts
+++ src/types/book.ts
@@ -8,17 +8,19 @@ export class Book {
   private _date: Date;
   private _score: number;
   private _imageUrl: string;
+  private _publisher: string;
 
   static validate(title: string, date: Date): boolean {
     return title !== '' && !isNaN(date.getTime());
   }
 
-  constructor(uid: string, title: string, synopsis: string = '', author: string, date: Date, score: number = 0.0, imageUrl: string = '') {
+  constructor(uid: string, title: string, synopsis: string = '', author: string, date: Date, publisher: string, score: number = 0.0, imageUrl: string = '') {
     this._uid = uid;
     this._title = title;
     this._synopsis = synopsis;
     this._author = author;
     this._date = date;
+    this._publisher = publisher;
     this._score = score;
     this._imageUrl = imageUrl;
   }
@@ -41,6 +43,10 @@ export class Book {
 
   get date(): Date {
     return this._date;
+  }
+
+  get publisher(): string {
+    return this._publisher;
   }
 
   get score(): number {
