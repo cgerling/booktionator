fce786a4183555d643896449e774126491515c79
Calvin Gerling <calvingerling@outlook.com>
2017-07-23 19:48:46 -0300

    feat(functions): add a search function
    temporary workaround function to exec a book search by title

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,36 @@
-const functions = require('firebase-functions');
+'use strict';
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp(functions.config().firebase);
+
+exports.search = functions.https.onRequest(function search(req, res) {
+  const searchTerm = req.query.q;
+  admin.database().ref('/books').once('value').then(function data(snapshot) {
+    const valAsArray = convertToArray(snapshot.val());
+    const result = filterByName(searchTerm, valAsArray);
+    res.json({ result: result });
+  }).catch(function onError(error) {
+    res.json(error);
+  });
+});
+
+function convertToArray(val) {
+  const array = [];
+  for (const key in val) {
+    array.push(Object.assign({ uid: key }, val[key]));
+  }
+
+  return array;
+}
+
+function filterByName(term = '', values = []) {
+  const search = createSearchExp(term);
+  return values.filter(n => search.test(n.title));
+}
+
+function createSearchExp(val = '') {
+  const exp = val.replace(/ /g, '.*');
+  return new RegExp(`.*${exp}.*`, 'gi')
+}
