99b4801283e8c8a00bea739effb52db59de478b3
Calvin Gerling <calvingerling@outlook.com>
2017-07-25 23:05:34 -0300

    fix(auth): redirect to login page when user is not logged

--- src/app/shared/guards/auth.guard.ts
+++ src/app/shared/guards/auth.guard.ts
@@ -1,29 +1,45 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, CanLoad, Route, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, CanLoad, Route, RouterStateSnapshot, Router } from '@angular/router';
 
 import { AuthService } from 'app/shared/services/auth.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate, CanActivateChild, CanLoad {
   private auth: AuthService;
+  private router: Router;
 
-  constructor(auth: AuthService) {
+  constructor(auth: AuthService, router: Router) {
     this.auth = auth;
+    this.router = router;
   }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean> {
-    return this.isLogged();
+    let logged = this.isLogged();
+    this.redirect(logged);
+    return logged;
   }
 
   canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean> {
-    return this.isLogged();
+    let logged = this.isLogged();
+    this.redirect(logged);
+    return logged;
   }
 
   canLoad(route: Route): Promise<boolean> {
-    return this.isLogged();
+    let logged = this.isLogged();
+    this.redirect(logged);
+    return logged;
   }
 
   private isLogged(): Promise<boolean> {
     return this.auth.isLogged();
+  }
+
+  private redirect(logged: Promise<boolean>): void {
+    logged.then((isLogged: boolean) => {
+      if (!isLogged) {
+        this.router.navigate(['access/login']);
+      }
+    });
   }
 }
