b09593c28342ed1aafb08715cf1ca61f6aef7a04
CiscoMossi <kikomossi@gmail.com>
2017-07-26 13:40:23 -0300

    feat(verify-account): add verify-account component #27
    Alerts user to verify their email account and show option to resend the verification email. #27

---
+++ src/app/shared/components/verify-account/verify-account.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { MdSnackBar } from '@angular/material';
+
+import { AuthService } from 'app/shared/services/auth.service';
+
+@Component({
+  selector: 'verify-account',
+  templateUrl: './verify-account.component.html',
+  styleUrls: ['./verify-account.component.scss']
+})
+export class VerifyAccountComponent implements OnInit {
+  private auth: AuthService;
+  private snackbar: MdSnackBar;
+
+  verified: boolean;
+  sent: boolean;
+  content: string;
+
+  constructor(auth: AuthService, snackbar: MdSnackBar) {
+    this.auth = auth;
+    this.snackbar = snackbar;
+    this.content = 'Verifique seu email';
+    this.sent = false;
+  }
+
+  sendEmail() : void{
+    this.auth.currentUser().then((user) => {
+      user.sendEmailVerification();
+      this.snackbar.open('Email enviado!', undefined,{
+        duration: 4000
+      });
+      this.sent = true;
+    }).catch((error) => {
+      //Tratar erros aqui
+    });
+  }
+
+  ngOnInit(): void {
+    this.auth.currentUser().then((user) => {
+      this.verified = user.emailVerified;
+    });
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { MdButtonModule, MdIconModule, MdInputModule, MdMenuModule, MdToolbarModule, MdCardModule } from '@angular/material';
+import { MdButtonModule, MdIconModule, MdInputModule, MdMenuModule, MdToolbarModule, MdCardModule, MdSnackBarModule } from '@angular/material';
 import { HttpModule } from '@angular/http';
 
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -11,6 +11,7 @@ import { DividerComponent } from './components/divider/divider.component';
 import { ResultViewerComponent } from './components/result-viewer/result-viewer.component';
 import { BookListComponent } from './components/book-list/book-list.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { VerifyAccountComponent } from './components/verify-account/verify-account.component';
 
 import { EmailValidatorDirective } from './directives/emailValidator.directive';
 import { TelValidatorDirective } from './directives/telValidator.directive';
@@ -24,9 +25,9 @@ import { StorageService } from './services/storage.service';
 import { AuthGuard } from './guards/auth.guard';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, MdButtonModule, MdIconModule, MdInputModule, MdMenuModule, MdToolbarModule, RouterModule, MdCardModule, HttpModule],
-  declarations: [CepValidatorDirective, DividerComponent, EmailValidatorDirective, NavbarComponent, PasswordToggleComponent, ResultViewerComponent, TelValidatorDirective, BookListComponent, SearchBarComponent],
-  providers: [AuthGuard, AuthService, LoaderService, StorageService, BookService],
-  exports: [CepValidatorDirective, DividerComponent, EmailValidatorDirective, NavbarComponent, BookListComponent, PasswordToggleComponent, ResultViewerComponent, TelValidatorDirective, SearchBarComponent]
+  imports: [CommonModule, FormsModule, MdButtonModule, MdIconModule, MdInputModule, MdMenuModule, MdToolbarModule, RouterModule, MdCardModule, MdSnackBarModule],
+  declarations: [CepValidatorDirective, DividerComponent, EmailValidatorDirective, NavbarComponent, PasswordToggleComponent, ResultViewerComponent, TelValidatorDirective, BookListComponent, SearchBarComponent, VerifyAccountComponent],
+  providers: [AuthGuard, AuthService, LoaderService, StorageService],
+  exports: [CepValidatorDirective, DividerComponent, EmailValidatorDirective, NavbarComponent, BookListComponent, PasswordToggleComponent, ResultViewerComponent, TelValidatorDirective, SearchBarComponent, VerifyAccountComponent]
 })
 export class SharedModule { }
