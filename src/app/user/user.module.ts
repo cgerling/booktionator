008f472af375f4ca958c6cdd9f2f216efe461d5b
Calvin Gerling <calvingerling@outlook.com>
2017-07-29 15:21:04 -0300

    style(user): add line breaks in imports

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,7 +3,9 @@ import { NgModule } from '@angular/core';
 import { UserRoutingModule } from './user.routing.module';
 
 @NgModule({
-  imports: [UserRoutingModule],
+  imports: [
+    UserRoutingModule
+  ],
   declarations: [],
   providers: [],
   exports: []
