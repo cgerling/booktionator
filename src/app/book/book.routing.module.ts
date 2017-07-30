f9934ad6f2c74047da65ae3de120420e0d1bc1fa
Calvin Gerling <calvingerling@outlook.com>
2017-07-30 01:01:54 -0300

    fix(book): add missing import to component route

--- src/app/book/book.routing.module.ts
+++ src/app/book/book.routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router'
 
+import { DetailsComponent } from './details/details.component';
 import { SearchComponent } from './search/search.component';
 import { SellComponent } from './sell/sell.component';
 
