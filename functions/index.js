23089a7f86bf6840db8221a09572fd69a10c550e
Calvin Gerling <calvingerling@outlook.com>
2017-08-01 09:43:13 -0300

    refactor(search): enclojure to search cloud function

--- functions/index.js
+++ functions/index.js
@@ -6,36 +6,41 @@ const cors = require('cors')({ origin: true });
 
 admin.initializeApp(functions.config().firebase);
 
-exports.search = functions.https.onRequest(function search(req, res) {
-  cors(req, res, () => {
-    let { q, limit } = req.query;
-    if (!q) res.status(400).json("Search term parameter is not optional");
-    if (!limit) limit = 12;
+let search;
+{
+  search = functions.https.onRequest(function onSearch(req, res) {
+    cors(req, res, () => {
+      let { q, limit } = req.query;
+      if (!q) res.status(400).json("Search term parameter is not optional");
+      if (!limit) limit = 12;
 
-    admin.database().ref('/books').limitToFirst(parseInt(limit)).once('value').then(function data(snapshot) {
-      const result = filterByName(q, toArray(snapshot.val()));
-      res.json({ term: q, result: result });
-    }).catch(function onError(error) {
-      res.json(error);
+      admin.database().ref('/books').limitToFirst(parseInt(limit)).once('value').then(function data(snapshot) {
+        const result = filterByName(q, toArray(snapshot.val()));
+        res.json({ term: q, result: result });
+      }).catch(function onError(error) {
+        res.json(error);
+      });
     });
   });
-});
 
-function toArray(val) {
-  const array = [];
-  for (const key in val) {
-    array.push(Object.assign({ uid: key }, val[key]));
+  function toArray(val) {
+    const array = [];
+    for (const key in val) {
+      array.push(Object.assign({ uid: key }, val[key]));
+    }
+
+    return array;
   }
 
-  return array;
+  function filterByName(term = '', values = []) {
+    const exp = searchExpression(term);
+    return values.filter(n => exp.test(n.title));
+  }
+
+  function searchExpression(val = '') {
+    const exp = val.replace(/ /g, '.*');
+    return new RegExp(`.*${exp}.*`, 'gi')
+  }
 }
 
-function filterByName(term = '', values = []) {
-  const exp = searchExpression(term);
-  return values.filter(n => exp.test(n.title));
-}
-
-function searchExpression(val = '') {
-  const exp = val.replace(/ /g, '.*');
-  return new RegExp(`.*${exp}.*`, 'gi')
-}
+exports.search = search;
