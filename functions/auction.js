42f510c310706d4002f5749f0980f1298ac4d4e3
Calvin Gerling <calvingerling@outlook.com>
2017-08-02 00:04:36 -0300

    feat(auction): add function to bid in auctions
    #37

--- functions/auction.js
+++ functions/auction.js
@@ -7,7 +7,27 @@ const utils = require('./utils');
 module.exports = function exportModule(functions, firebase, cors) {
   const bid = functions.https.onRequest(function onBuy(req, res) {
     cors(req, res, () => {
+      const { auction, bid, user } = req.body;
+      if (utils.isEmpty(auction)) res.json('Auction parameter is not optional');
+      if (utils.isEmpty(bid)) res.json('Bid value is not optional');
+      if (utils.isEmpty(user)) res.json('User identifier is not optional');
 
+      const auctionRef = firebase.database().ref(`/auctions/${auction}`);
+      auctionRef.once('value').then(snapshot => {
+        const auction = snapshot.val();
+        if (new Date() > auction.due) res.json("Auction closed.");
+      });
+
+      auctionRef.child('bids').once('value').then(snapshot => {
+        const bids = utils.toArray(snapshot.val());
+        const biggestBid = utils.maxValue(bids, (a, b) => b.value - a.value);
+        if (bid <= biggestBid) res.json('There is already a bigger or equivalent bid.');
+      });
+
+      firebase.database().ref(`/auctions/${auction}/bids/${user}`).set({
+        value: bid,
+        at: new Date()
+      });
     });
   });
 
