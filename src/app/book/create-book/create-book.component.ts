2e5d8546f8e83b9482669f6f4036e131dbe569b5
Calvin Gerling <calvingerling@outlook.com>
2017-08-10 08:08:32 -0300

    feat(create-book): use the new service to get the book cover

--- src/app/book/create-book/create-book.component.ts
+++ src/app/book/create-book/create-book.component.ts
@@ -3,6 +3,8 @@ import { Http } from '@angular/http';
 
 import { AngularFireDatabase } from 'angularfire2/database';
 
+import { GoogleBooks } from 'app/shared/services/googleBooks.service';
+
 import { Book } from 'types/book';
 
 @Component({
@@ -16,25 +18,35 @@ export class CreateBookComponent {
   date: Date;
   publisher: string;
   description: string;
+  imageUrl: string;
 
   private database: AngularFireDatabase;
   private http: Http;
+  private api: GoogleBooks;
 
-  constructor(http: Http, database: AngularFireDatabase) {
+  constructor(http: Http, database: AngularFireDatabase, api: GoogleBooks) {
+    this.api = api;
     this.database = database;
     this.http = http;
   }
 
   save(): void {
-    //TODO: Google Books API integration
-
-    this.database.list('/books').push({
-      title: this.title,
-      author: this.author,
-      date: this.date,
-      score: 0,
-      publisher: this.publisher,
-      description: this.description
+    this.api.volume(this.title).subscribe((result) => {
+      const volume = result.json();
+      const imageDefault = '/assets/logo.png';
+      const book = volume.items && volume.items.length > 0 ? volume.items[0] : imageDefault;
+      const imageUrl = book.imageLinks ? book.imageLinks.thumbnail ? book.imageLinks.thumbnail : imageDefault : imageDefault;
+      this.database.list('/books').push({
+        title: this.title,
+        author: {
+          name: this.author
+        },
+        date: this.date,
+        score: 0,
+        publisher: this.publisher,
+        description: this.description,
+        imageUrl: imageUrl
+      });
     });
   }
 }
