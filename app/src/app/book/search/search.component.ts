7bafb44aa6c9bdb23dc59264a374d4a46486d31c
Diogo Korok <korokdiogo@gmail.com>
2017-12-05 19:30:08 -0200

    gambeta(search): move logic to the front using angularfire

--- app/src/app/book/search/search.component.ts
+++ app/src/app/book/search/search.component.ts
@@ -1,10 +1,14 @@
 import { Component, ViewChild, OnInit, Inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+import { AngularFireDatabase } from 'angularfire2/database';
+
 import { SearchBarComponent } from '../../shared/components/search-bar/search-bar.component';
 import { BookService } from '../../shared/services/book.service';
 
 import { Book } from 'types/book';
+import { Book as BookInterface } from 'types/types';
+import { log } from 'util';
 
 @Component({
   selector: 'search',
@@ -14,22 +18,24 @@ import { Book } from 'types/book';
 export class SearchComponent implements OnInit {
   private bookService: BookService;
   private route: ActivatedRoute;
+  private dbFirebase: AngularFireDatabase;
 
   mode: string;
 
   @ViewChild(SearchBarComponent)
   searchBar: SearchBarComponent;
 
-  books: Book[];
+  books: BookInterface[];
   bookLink: string;
 
   instructions: string;
   modes: { [key: string]: string };
   instructionsMessages: { [key: string]: string };
 
-  constructor(bookService: BookService, route: ActivatedRoute) {
+  constructor(bookService: BookService, route: ActivatedRoute, db: AngularFireDatabase) {
     this.bookService = bookService;
     this.route = route;
+    this.dbFirebase = db;
 
     this.modes = {
       'sell': '/book/sell',
@@ -62,8 +68,27 @@ export class SearchComponent implements OnInit {
   }
 
   search(): void {
-    this.bookService.search(this.searchBar.term).subscribe((res) => {
-      this.books = res.json().result.map((val) => this.mapBook(val));
+    // this.bookService.search(this.searchBar.term).subscribe((res) => {
+    //   this.books = res.json().result.map((val) => this.mapBook(val));
+    // });
+    this.getBooks();
+  }
+
+  getBooks(): void {
+    let self = this;
+    this.dbFirebase.list('books', (ref) => ref).valueChanges().subscribe(values => {
+      self.books = values.map(value => {
+        return <BookInterface>{
+          uid: value['$key'],
+          title: value['title'],
+          description: value['description'],
+          author: value['author'],
+          date: new Date(value['date']),
+          publisher: value['publisher'],
+          score: value['score'],
+          image: value['image']
+        };
+      }).filter(book => book.title.toLowerCase().includes(this.searchBar.term.toLowerCase()));
     });
   }
 
